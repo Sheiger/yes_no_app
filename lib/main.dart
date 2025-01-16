3f40045f906342acefab9628e79c29931548baac
Sheiger <emersonquispe2808@gmail.com>
2025-01-15 22:11:43 -0500

    clean code

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Yes No App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+        ),
+        body: Center(
+          child: FilledButton.tonal(
+            onPressed: (){}, 
+            child: const Text('Click me')
+            ),
+        ),
+      ),
+    );
+  }
+}
+
+
